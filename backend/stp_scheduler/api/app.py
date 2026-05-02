c78115dc9df00bcc24534ca86d4e688d8a5b4d57
Logan Endes <log@csh.rit.edu>
2026-05-02 14:46:41 -0400

    Update CORS headers to include Access-Control-Allow-Origin for enhanced cross-origin requests

--- backend/stp_scheduler/api/app.py
+++ backend/stp_scheduler/api/app.py
@@ -93,7 +93,7 @@ app.add_middleware(
     allow_origins=origins,
     allow_credentials=True,
     allow_methods=["GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS"],
-    allow_headers=["Authorization", "Content-Type"],
+    allow_headers=["Authorization", "Content-Type", "Access-Control-Allow-Origin"],
 )
 
 
